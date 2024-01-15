9e1f4e48daf6463beda81d203b4f69012333d0a9
Arnab Kumar Das <78913766+Arnab-batsy@users.noreply.github.com>
2024-01-16 02:14:34 +0530

    Update

--- Practice/src/com/arnab/Array/Smallest.java
+++ Practice/src/com/arnab/Array/Smallest.java
@@ -5,6 +5,7 @@ public class Smallest {
     public static void main(String[] args) {
         int[] arr= {2,5,15,3,98,10,7,9};
         System.out.println(smallest(arr));
+        System.out.println(greatest(arr));
     }
 
     private static int smallest(int[] arr) {
@@ -15,5 +16,13 @@ public class Smallest {
                 min = j;
         }
         return min;
+    }
+    private static int greatest(int[] arr) {
+        int max= arr[0];
+        for (int j : arr) {
+            if (max < j)
+                max = j;
+        }
+        return max;
     }
 }
